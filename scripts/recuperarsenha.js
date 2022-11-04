0aa046877077048bae2c4ba04efe15b7751775aa
Davi <davijustinocouto@gmail.com>
2022-11-03 22:34:26 -0300

    Novas paginas criadas: 🚪Log in: Falta adicionar o validar o campo de entrada como e-mail 🚪Recuperar Senha: Falta adicionar o validar o campo de entrada como e-mail

---
+++ scripts/recuperarsenha.js
@@ -0,0 +1,4 @@
+const enviarRecuperacao = document.getElementById("enviarRecuperar")
+enviarRecuperacao.onclick = function() {
+    return alert("Foi enviada uma mensagem de recuperação de senha para o seu e-mail")
+}
