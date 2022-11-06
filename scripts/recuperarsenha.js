b3785d9cff5e58dd2c11db3d29a4ce9f2d088960
Davi <davijustinocouto@gmail.com>
2022-11-06 16:45:31 -0300

    Paginas formatadas e comentadas: Log in, recuperar senha

--- scripts/recuperarsenha.js
+++ scripts/recuperarsenha.js
@@ -3,7 +3,7 @@
 // 	swal("Recuperação de senha enviada!", "Cheque sua caixa de entrada, lixeira e(ou) spam", "success");
 // })	
 
-document.getElementById('sweetalert').addEventListener('click',function validaCadastro(evt) {
+document.getElementById('sweetalert').addEventListener('click', function validaCadastro(evt) {
 	var email = document.getElementById('email');
 	var filtro = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
 	var contErro = 0;
@@ -14,8 +14,8 @@ document.getElementById('sweetalert').addEventListener('click',function validaCa
 	if (email.value == "") {
 		swal("E-mail incompleto ou errado", "Corrija seu e-mail", "error");
 	} else if (filtro.test(email.value)) {
-	swal("Recuperação de senha enviada!", "Cheque sua caixa de entrada, lixeira e(ou) spam", "success");
-} else {
+		swal("Recuperação de senha enviada!", "Cheque sua caixa de entrada, lixeira e(ou) spam", "success");
+	} else {
 		caixa_email.innerHTML = "Formato do E-mail inválido";
 		caixa_email.style.display = 'block';
 		contErro += 1;
